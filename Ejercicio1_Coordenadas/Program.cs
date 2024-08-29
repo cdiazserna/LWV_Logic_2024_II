bcf80fb401d0a52ef6dfd6cd25c67538447fb502
Carlor Jr Diaz <cjunior90@gmail.com>
2024-08-28 21:42:05 -0500

    Guardamos avances de mi código

--- Ejercicio1_Coordenadas/Program.cs
+++ Ejercicio1_Coordenadas/Program.cs
@@ -1,7 +1,25 @@
-﻿internal class Program
+﻿public class Program
 {
+    //Significan que es para comentar en línea:
+
+    /* Comentarios en bloque
+     * fasgvadsgbasdbn
+     * dgbasbsdrnr
+     * sdfn srmnrtsedt
+     * sfnsfmnsfmndfs
+     * sfmnsfmn
+     */
+
     private static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        //Lea las coordenadas de dos puntos y que muestre la distancia entre los dos puntos. 
+
+        //Datos de Entrada
+        //Declarar las variables, en este caso son 4:
+        double x1, x2, y1, y2, d;
+
+        //Vamos a inicializar mis variables pidiendoles sus valores por pantalla:
+        Console.Write("Por favor ingrese el valor de x1: ");
+        x1 = Convert.ToDouble(Console.ReadLine()); //Esta es la forma de escribir en pantalla
     }
 }
