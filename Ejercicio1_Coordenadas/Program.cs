6ca9944f63a5beba947497a64a7fd3cbfe6bb34d
Carlor Jr Diaz <cjunior90@gmail.com>
2024-08-30 21:34:49 -0500

    Ejercicio 1 terminado!

--- Ejercicio1_Coordenadas/Program.cs
+++ Ejercicio1_Coordenadas/Program.cs
@@ -19,7 +19,49 @@
         double x1, x2, y1, y2, d;
 
         //Vamos a inicializar mis variables pidiendoles sus valores por pantalla:
-        Console.Write("Por favor ingrese el valor de x1: ");
-        x1 = Convert.ToDouble(Console.ReadLine()); //Esta es la forma de escribir en pantalla
+        Console.Write("Por favor ingrese el valor de x1: "); //Esta es la forma de escribir en pantalla
+        x1 = Convert.ToDouble(Console.ReadLine()); //Esta es la forma de leer en pantalla
+
+        Console.Write("Por favor ingrese el valor de y1: ");
+        y1 = Convert.ToDouble(Console.ReadLine());
+
+        Console.Write("Por favor ingrese el valor de x2: ");
+        x2 = Convert.ToDouble(Console.ReadLine());
+
+        Console.Write("Por favor ingrese el valor de y2: ");
+        y2 = Convert.ToDouble(Console.ReadLine());
+
+        Console.WriteLine(" ");
+        Console.WriteLine(" ");
+
+        //Proceso - cálculo de la variable "d";
+        double x = (x2 - x1) * (x2 - x1);
+        double y = (y2 - y1) * (y2 - y1);
+
+        double suma = x + y;
+
+        //La clase Math tiene muchos métodos, entre ellos el método Sqrt, este señor recibe un parámetro dentro de sus ( ), ese parámetro es el número que yo le mando al método para que me saque la Raíz Cuarada!
+        d = Convert.ToDouble(Math.Sqrt(suma));
+
+        //Forma más primitiva de concatenar texto a través del símbolo "+"
+        Console.WriteLine("Concatenar con +");
+        Console.Write("La distancia entre los pares ordenados (" + x1 + "," + y1 + ") y (" + x2 + "," + y2 + ") es: " + d);
+
+        Console.WriteLine(" ");
+        Console.WriteLine(" ");
+
+        //Forma de concatenar a través del símbolo "$" se llama Interpolación:
+        Console.WriteLine("Concatenar con $");
+        Console.Write($"La distancia entre los pares ordenados ({x1},{y1}) y ({x2},{y2}) es: {d}");
+
+        Console.WriteLine(" ");
+        Console.WriteLine(" ");
+
+        Console.WriteLine("Concatenar con String.Format");
+        //Forma de concatenar con la clase String.Format:
+        Console.Write(String.Format("La distancia entre los pares ordenados ({0},{1}) y ({2},{3}) es: {4}", x1, y1, x2, y2, d));
+
+        Console.WriteLine(" ");
+        Console.WriteLine(" ");
     }
 }
