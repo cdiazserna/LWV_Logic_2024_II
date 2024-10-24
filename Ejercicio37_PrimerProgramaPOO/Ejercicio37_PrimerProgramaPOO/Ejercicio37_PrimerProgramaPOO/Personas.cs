1f5e3b95795861ea606c0a86d9360dbf78ff93f4
Carlos Jr Diaz S <cjunior90@gmail.com>
2024-10-23 21:37:51 -0500

    Clase Persona.cs creada con sus propiedades y métodos

---
+++ Ejercicio37_PrimerProgramaPOO/Ejercicio37_PrimerProgramaPOO/Ejercicio37_PrimerProgramaPOO/Personas.cs
@@ -0,0 +1,33 @@
+﻿namespace Ejercicio37_PrimerProgramaPOO
+{
+    public class Personas
+    {
+        //Propiedades de mi clase Personas:
+        public string Nombre { get; set; } //Forma de definir mis propiedades getters and setters
+
+        public int Edad { get; set; }
+
+
+        //Método de mi clase Personas:
+
+        //Esta es la forma de crear un método en C#:
+        //Modificador de acceso + tipo de dato que retorna el método + nombre del método + sección de parámetros ()
+        public void Presentar() //Void significa que el método no retorna NADA
+        {
+            Console.WriteLine($"Hola! Mi nombre es {Nombre} y tengo {Edad} años.");
+        }
+
+        //Segundo método para validar si la persona es mayor de edad o no:
+        public void EsMayorDeEdad()
+        {
+            if (Edad >= 18)
+            {
+                Console.WriteLine("Soy mayor de edad.");
+            }
+            else
+            {
+                Console.WriteLine("Soy menor de edad.");
+            }
+        }
+    }
+}
---
+++ Ejercicio37_PrimerProgramaPOO/Ejercicio37_PrimerProgramaPOO/Ejercicio37_PrimerProgramaPOO/Program.cs
@@ -0,0 +1,8 @@
+﻿public class Program
+{
+    private static void Main(string[] args)
+    {
+        /*37.Desarrollar un programa en C# utilizando los conceptos básicos de la Programación Orientada a Objetos (POO) y bucles para permitir la creación y presentación de N personas. El programa debe solicitar al usuario el número de personas que desea ingresar, obtener los datos de cada una (nombre y edad), y luego mostrar una presentación de cada persona junto con la verificación de si es mayor de edad.*/
+        int num = 0;
+    }
+}
